f18162e889bf6be487c4bc50a500d7a43d599a73
Sabathick <erickick13@gmail.com>
2022-02-28 15:37:20 -0300

    Q2 feita, apenas detalhes de texto para arrumar

--- AD1-1/Q1.py
+++ AD1-1/Q1.py
@@ -1,13 +1,64 @@
 #Programa para retornar o troco dos valores comas maiores cédulas possiveis
 
 val = []
+entrada = 0
 #Obtendo as entradas e validando se os valores são inteiros
-while i != "":
-    val[i] = int(input("Digite um valor: "))
-    if val[i] <= 0:
-        print("Valor menor que zero!")
-        exit()
+while entrada >= 0:
+    entrada = int(input("Digite um valor: "))
+    if entrada > 0:
+        val.append(entrada)
     else:
-        i = i +1
+        print("Valor inválido!")
+        break
 
+print(val)
 
+for i in val:
+    print("Trocando "+val[i]+" em:\n")
+
+    #Teste para notas de 100
+    result = val[i]/100
+    resto = val[i] % 100
+    if result == 1:
+        print(result+" nota de 100 reais")
+    if result > 1:
+        print(result+" notas de 100 reais")
+    #Teste para notas de 50
+    result = resto/50
+    resto = result % 50
+    if result == 1:
+        print(result+" nota de 50 reais")
+    if result > 1:
+        print(result+" notas de 50 reais")
+    #Teste para notas de 20
+    result = resto/20
+    resto = result % 20
+    if result == 1:
+        print(result+" nota de 20 reais")
+    if result > 1:
+        print(result+" notas de 20 reais")
+    #Teste para notas de 10
+    result = resto/10
+    resto = result % 10
+    if result == 1:
+        print(result+" nota de 10 reais")
+    if result > 1:
+        print(result+" notas de 10 reais")
+    #Teste para notas de 5
+    result = resto/5
+    resto = result % 5
+    if result == 1:
+        print(result+" nota de 5 reais")
+    if result > 1:
+        print(result+" notas de 5 reais")
+    #Teste para notas de 2
+    result = resto/2
+    resto = result % 2
+    if result ==1:
+        print(result+" nota de 2 reais")
+    if result > 1:
+        print(result+" notas de 2 reais")
+    if resto == 1:
+        print(resto+" moeda de 1 real")
+    if result > 1:
+        print(resto+" moedas de 1 real")
--- AD1-1/Q2.py
+++ AD1-1/Q2.py
@@ -1,1 +1,33 @@
-#Fazer testes, não foi especificado corretamente, algo a pensar
+#A primeira linha é a quantidade de testes qtdTestes
+#A segunda linha é a quantidade de valores de cada teste qtdValoresTeste
+#Na terceira um valor mínimo de um intervalo de valores
+#Na quarta, o valor máximo do intervalo
+
+valores = []
+abaixo = 0
+acima = 0
+dentro = 0
+soma = 0
+val = None
+qtdTestes = int(input("Digite a quantidade de testes: "))
+qtdValoresTeste = int(input("Digite a quantidade de valores em cada teste: "))
+min = float(input("Digite o valor mínimo do intervalo: "))
+max = float(input("Digite o valor máximo do intervalo: "))
+
+for i in range(qtdValoresTeste):
+    val = float(input("Insira o "+str(i)+"º do teste: "))
+    valores.append(val)
+    if valores[i] < min:
+        abaixo = abaixo + 1
+    elif valores[i] > min and valores[i] < max:
+        dentro = dentro + 1
+        soma = soma + valores[i]
+    elif valores[i] > max:
+        acima = acima + 1
+    else:
+        break 
+
+for i in range(qtdTestes):
+    print("Teste "+str(i)+":")
+    print("Abaixo do Intervalo: "+str(abaixo)+", No intervalo: "+str(dentro)+",\n Acima do Intervalo: "+str(acima)+".")
+    print("Soma dos Valores Dentro do Intervalo: "+str(soma))

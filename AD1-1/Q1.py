81de40f9aa0336eb0d62655359e0089f4b0d4105
Sabathick <erickick13@gmail.com>
2022-02-28 10:59:55 -0300

    Validacao das entradas

--- AD1-1/Q1.py
+++ AD1-1/Q1.py
@@ -1,1 +1,13 @@
 #Programa para retornar o troco dos valores comas maiores cédulas possiveis
+
+val = []
+#Obtendo as entradas e validando se os valores são inteiros
+while i != "":
+    val[i] = int(input("Digite um valor: "))
+    if val[i] <= 0:
+        print("Valor menor que zero!")
+        exit()
+    else:
+        i = i +1
+
+

bb9e3331c5b1fdf96ab83cedf67c9e5e0c437da6
Erick Pereira Martins <erickick13@gmail.com>
2022-03-01 18:25:52 -0300

    Add files via upload
    Foram encontrados erros na formatação do output

--- AD1-1/Q2.py
+++ AD1-1/Q2.py
@@ -14,20 +14,21 @@ qtdValoresTeste = int(input("Digite a quantidade de valores em cada teste: "))
 min = float(input("Digite o valor mínimo do intervalo: "))
 max = float(input("Digite o valor máximo do intervalo: "))
 
-for i in range(qtdValoresTeste):
-    val = float(input("Insira o "+str(i)+"º do teste: "))
-    valores.append(val)
-    if valores[i] < min:
-        abaixo = abaixo + 1
-    elif valores[i] > min and valores[i] < max:
-        dentro = dentro + 1
-        soma = soma + valores[i]
-    elif valores[i] > max:
-        acima = acima + 1
-    else:
-        break 
-
-for i in range(qtdTestes):
-    print("Teste "+str(i)+":")
+for j in range(0, qtdTestes):
+    for i in range(0, qtdValoresTeste):
+        val = float(input("Insira o "+str(i+1)+"º valor do "+str(j+1)+"º teste: "))
+        valores.append(val)
+        if valores[i] < min:
+            abaixo = abaixo + 1
+        elif valores[i] > min and valores[i] < max:
+            dentro = dentro + 1
+            soma = float(soma + float(valores[i]))
+        elif valores[i] > max:
+            acima = acima + 1
+    print("Teste "+str(j+1)+":")
     print("     Abaixo do Intervalo: "+str(abaixo)+", No intervalo: "+str(dentro)+",\n Acima do Intervalo: "+str(acima)+".")
     print("     Soma dos Valores Dentro do Intervalo: "+str(soma))
+    abaixo = 0
+    soma = 0
+    dentro = 0
+    valores = []

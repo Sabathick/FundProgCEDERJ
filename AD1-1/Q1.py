783dcfab47d5a46ad22284a92b8dd70252f700e8
Erick Pereira Martins <erickick13@gmail.com>
2022-03-01 12:06:39 -0300

    Add files via upload
    Exercício completo

--- AD1-1/Q1.py
+++ AD1-1/Q1.py
@@ -2,63 +2,82 @@
 
 val = []
 entrada = 0
+i = 0
 #Obtendo as entradas e validando se os valores são inteiros
-while entrada >= 0:
-    entrada = int(input("Digite um valor: "))
+while i >= 0:
+    entrada = input("Digite um valor: ")
+    if entrada != '':
+        entrada = int(entrada)
+    else:
+        break
     if entrada > 0:
         val.append(entrada)
+        i += 1
     else:
         print("Valor inválido!")
         break
 
 print(val)
 
-for i in val:
-    print("Trocando "+val[i]+" em:\n")
+for i in range(0,len(val)):
+    print("Trocando "+str(val[i])+" em:")
 
     #Teste para notas de 100
-    result = val[i]/100
-    resto = val[i] % 100
-    if result == 1:
-        print(result+" nota de 100 reais")
-    if result > 1:
-        print(result+" notas de 100 reais")
+    resto = int(val[i] % 100)
+
+    if val[i] >= 100:
+        result = int(val[i]/100)
+        if result == 1:
+            print("     "+str(result)+" nota de 100 reais")
+        if result > 1:
+            print("     "+str(result)+" notas de 100 reais")
+
     #Teste para notas de 50
-    result = resto/50
-    resto = result % 50
-    if result == 1:
-        print(result+" nota de 50 reais")
-    if result > 1:
-        print(result+" notas de 50 reais")
+    if resto >= 50:
+        result = int(resto/50)
+        resto = int(resto % 50)
+        if result == 1:
+            print("     "+str(result)+" nota de 50 reais")
+        if result > 1:
+            print("     "+str(result)+" notas de 50 reais")
+
     #Teste para notas de 20
-    result = resto/20
-    resto = result % 20
-    if result == 1:
-        print(result+" nota de 20 reais")
-    if result > 1:
-        print(result+" notas de 20 reais")
+    if resto >= 20:
+        result = int(resto/20)
+        resto = int(resto % 20)
+        if result == 1:
+            print("     "+str(result)+" nota de 20 reais")
+        if result > 1:
+            print("     "+str(result)+" notas de 20 reais")
+    
     #Teste para notas de 10
-    result = resto/10
-    resto = result % 10
-    if result == 1:
-        print(result+" nota de 10 reais")
-    if result > 1:
-        print(result+" notas de 10 reais")
+    if resto >= 10:
+        result = int(resto/10)
+        resto = int(resto % 10)
+        if result == 1:
+            print("     "+str(result)+" nota de 10 reais")
+        if result > 1:
+            print("     "+str(result)+" notas de 10 reais")
+
     #Teste para notas de 5
-    result = resto/5
-    resto = result % 5
-    if result == 1:
-        print(result+" nota de 5 reais")
-    if result > 1:
-        print(result+" notas de 5 reais")
+    if resto >= 5:
+        result = int(resto/5)
+        resto = int(resto % 5)
+        if result == 1:
+            print("     "+str(result)+" nota de 5 reais")
+        if result > 1:
+            print("     "+str(result)+" notas de 5 reais")
+    
     #Teste para notas de 2
-    result = resto/2
-    resto = result % 2
-    if result ==1:
-        print(result+" nota de 2 reais")
-    if result > 1:
-        print(result+" notas de 2 reais")
-    if resto == 1:
-        print(resto+" moeda de 1 real")
-    if result > 1:
-        print(resto+" moedas de 1 real")
+    if resto >= 2:
+        result = int(resto/2)
+        resto = int(resto % 2)
+        if result ==1:
+            print("     "+str(result)+" nota de 2 reais")
+        if result > 1:
+            print("     "+str(result)+" notas de 2 reais")
+    if resto >= 1:
+        if resto == 1:
+            print("     "+str(result)+" moeda de 1 real")
+        if result > 1:
+            print("     "+str(result)+" moedas de 1 real")

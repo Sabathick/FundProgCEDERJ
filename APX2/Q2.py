8d9634704dcf28e8363da457f987c3f1326dd04d
Sabathick <erickick13@gmail.com>
2022-05-28 09:46:48 -0300

    tentativa

--- APX2/Q2.py
+++ APX2/Q2.py
@@ -1,10 +1,12 @@
 def procuraPalvra(arq,palavra):
+    contador = 0
     conteudo = open(arq, 'r')
     for linha in conteudo:
         dados_da_linha = linha.split()
+        contador = contador + 1
         for dado in dados_da_linha:
             if dado == palavra:
-                print("Linha "+ linha + ", Palavra " + dado + " nesta linha!")
+                print("Linha "+ str(contador) + ", Palavra " + str(dados_da_linha.index(dado)+1) + " nesta linha!")
     conteudo.close()
 
 def main():
--- APX2/Q3.py
+++ APX2/Q3.py
@@ -1,34 +1,61 @@
 import os
+from pathlib import Path
 
-def verificaPrimo(num):
+def mostrar(nm):
+    dados = open(nm, "r")
+    for linha in dados:
+        print("\t", linha.strip())
+    dados.close()
+    print()
+    return None
+
+def verifica_primo(nums):
     divisores = 0
-    if num > 0:
-        for i in range(num):
-            if num%(i+1) == 0:
-                divisores +=1
-    else:
-        return False
-    if divisores == 2:
-        return True
-    else:
-        return False
+    result = []
+    for num in nums:
+        num = int(num)
+        if num > 0:
+            for i in range(num):
+                if num%(i+1) == 0:
+                    divisores +=1
+        else:
+            result.append(False)
+        if divisores == 2:
+            result.append(True)
+        else:
+            result.append(False)
+        
+        divisores = 0
+    return result
 
-def procuraPrimo(doc):
+def procura_primo(doc):
     print("Conteúdo do Arquivo "+doc+":")
-    conteudo = open(doc, 'r+')
+    mostrar(doc)
+    conteudo = open(doc,'r+')
     temp = open("temp.txt", 'w+')
+    caminho = Path("temp.txt").absolute()
     for linha in conteudo:
         dados = linha.split()
-        for dado in dados:
-            if verificaPrimo(dado) == False in linha:
-                temp.write(linha)
+        if any(verifica_primo(dados)):
+            pass
+        else:
+            temp.write(str(dados)+'\n')
+
     
-    os.replace(temp, doc)            
     print("Conteúdo do Arquivo "+doc+" após eventuais remoções:")
+    os.replace(caminho,doc)
+    for linha in conteudo:
+        linha = linha.replace('[','')
+        linha = linha.replace(',',' ')
+        print(linha)
+        
     conteudo.close()
+    temp.close()
+                   
+    return None
 
 def main():
     arq = input("Insira o nome do arquivo: ")
-    procuraPrimo(arq)
+    procura_primo(arq)
 
 main()
